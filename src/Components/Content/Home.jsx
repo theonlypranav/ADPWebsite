fb830d9a19be203b42e8eb0673b6ee9e0b309729
Krish-hashhh <f20230887@pilani.bits-pilani.ac.in>
2024-07-23 16:49:03 +0530

    fonts home page footer and wa link

--- src/Components/Content/Home.jsx
+++ src/Components/Content/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { AiFillGithub } from 'react-icons/ai';
 import { FaXTwitter } from "react-icons/fa6";
-import { FaLinkedinIn, FaInstagram } from 'react-icons/fa';
+import { FaLinkedinIn, FaInstagram,FaWhatsapp } from 'react-icons/fa';
 import img from '../../assets/ADPLogoNewGrad.png';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
@@ -21,16 +21,20 @@ function Home() {
     {
       link: 'https://www.instagram.com/adp_bitspilani/',
       icon: <FaInstagram className="icon-glow text-silver-700 hover:text-silver-500" />,
-    }
+    },
+    {
+      link: 'http://wasap.my/918000695988',
+      icon: <FaWhatsapp className="icon-glow text-silver-700 hover:text-silver-500" />,
+    },
   ];
 
   return (
     <div id='Home' className='bg-custom-light text-black dark:bg-custom-dark dark:text-white lg:px-56 lg:py-0 px-10 py-20 text-center gap-5 lg:text-start flex lg:flex-row flex-col-reverse justify-between lg:gap-28 items-center'>
       <div className='h-full lg:py-40 flex flex-col justify-center lg:items-start items-center'>
-        <h1 data-aos='fade-right' className='text-[52px] font-semibold mb-8 leading-normal uppercase'>
+        <h1 data-aos='fade-right'style={{ fontFamily: 'Anton' }} className='text-[58px] font-bold mb-8 leading-normal uppercase'>
           Welcome To <span className='text-silver-700'>The Department of Art, Design and Publicity</span>
         </h1>
-        <p data-aos='fade-left'>We are a group of art enthusiasts who enjoy working together on awesome pieces of art for the festivals Oasis and APOGEE, the grand cultural and technical festivals of BITS Pilani, Pilani Campus. We make the stage shine with our beautiful backdrops and awe-spire the festival visitors with our amazing structures. We also handle the publicity of Oasis and APOGEE</p>
+        <p data-aos='fade-left'>A collective of passionate artists, we are the visionaries behind the stunning aesthetics of Oasis and APOGEE, the grand cultural and technical fest of BITS Pilani . From breathtaking backdrops to mind-blowing structures, we craft the ambiance that defines these iconic festivals . We are the architects of the visual narrative, infusing every corner with creativity and innovation . Beyond the artistic realm, we are the driving force behind the publicity of these flagship events . Our canvas is the stage, and our colors are imagination. We create more than just visuals; we craft experiences.</p>
         <div data-aos='fade-up' className='flex mt-8 gap-2'>
           <div className='flex items-center justify-center'>
             <div className='flex space-x-2'>
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { AiFillGithub } from 'react-icons/ai';
 import { FaXTwitter } from "react-icons/fa6";
-import { FaLinkedinIn, FaInstagram } from 'react-icons/fa';
+import { FaLinkedinIn, FaInstagram, FaWhatsapp } from 'react-icons/fa';
 
 function Footer() {
 
@@ -13,21 +13,26 @@ function Footer() {
     {
       link: 'https://www.instagram.com/adp_bitspilani/',
       icon: <FaInstagram className="icon-glow text-silver-700 hover:text-silver-500" />,
-    }
+    },
+    {
+      link: 'http://wasap.my/918000695988',
+      icon: <FaWhatsapp className="icon-glow text-silver-700 hover:text-silver-500" />,
+    },
     ];
 
     return (
         <footer className='bg-neutral-900 text-white border-t border-silver-700 p-5 lg:px-48 flex justify-between'>
             <div className='flex flex-col md:flex-row gap-10'>
                 <div>
-                    <h2 className='text-[22px] font-semibold text-silver-700 py-2 uppercase'>Contact</h2>
-                    <a href="mailto:chiragjain.jain7@gmail.com" className='text-[16px] hover:text-silver-700 transition-all duration-150 ease-in-out cursor-pointer'><b>Email</b>: adp@bits-oasis.org; adp@bits-apogee.org</a>
-                    <p className='text-[16px] pb-5 hover:text-silver-700 transition-all duration-150 ease-in-out'>Phone: +91-8302421632</p>
+                    <h2 className='text-[24px] font-normal text-silver-700 py-2 uppercase'style={{ fontFamily: 'Anton',letterSpacing: '0.7px' }}>Contact</h2>
+                    <a href="mailto:adp@bits-oasis.org" className='text-[16px] hover:text-silver-700 transition-all duration-150 ease-in-out cursor-pointer'><b>Email</b>: adp@bits-oasis.org</a>
+                    <br />
+                    <a href="mailto:adp@bits-apogee.org" className='text-[16px] hover:text-silver-700 transition-all duration-150 ease-in-out cursor-pointer'><b>Email</b>: adp@bits-apogee.org</a>
                 </div>
             </div>
             <div className='flex flex-col md:flex-row gap-10'>
                 <div>
-                    <h2 className='text-[22px] font-semibold text-silver-700 py-2 uppercase'>Follow Us On</h2>
+                    <h2 className='text-[24px] font - normal text-silver-700 py-2 uppercase'style={{ fontFamily: 'Anton',letterSpacing: '0.7px' }}>Connect With Us On</h2>
                     <div className='flex space-x-4'>
                         {social.map((item, index) => (
                             <a key={index} target="_blank" rel="noopener noreferrer" href={item.link} className='hover:text-silver-700 transition-all duration-150 ease-in-out'>
@@ -37,7 +42,7 @@ function Footer() {
                             </a>
                         ))}
                     </div>
-                </div>
+                 </div>
             </div>
         </footer>
     );
