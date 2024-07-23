198c7b68e760f60c8487024ad1db7b328f16b365
YoursAJ24 <148056802+YoursAJ24@users.noreply.github.com>
2024-07-23 17:02:32 +0530

    Update ProjectsList.js

--- src/All_Lists/ProjectsList.js
+++ src/All_Lists/ProjectsList.js
@@ -18,7 +18,7 @@ const ProjectsList = [
     {
         id: 2,
         title: 'APOGEE',
-        link: '../Components/Apogee/Apogee24/Apogee24.html',
+        link: '../../public/Apogee24.html',
         image: img2,
         instagram: 'https://www.instagram.com/bitsapogee/',
         linkedin: 'https://www.linkedin.com/company/apogee-bits-pilani/',
