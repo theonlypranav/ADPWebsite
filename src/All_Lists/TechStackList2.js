955b5feaf1f9fbec418f9a69e911fa9807ef2336
YoursAJ24 <148056802+YoursAJ24@users.noreply.github.com>
2024-07-24 13:06:42 +0530

    linked slideshow images with endpoint page

--- src/All_Lists/TechStackList2.js
+++ src/All_Lists/TechStackList2.js
@@ -7,61 +7,71 @@ const TechStackList2 = [
         id: 1,
         image: HTML,
         title: "Shreyansh Vanjani",
-        subtitle: "StuCCAn, Oasis '24"
+        subtitle: "StuCCAn, Oasis '24",
+        link: '../endpoint'
     },
     {
         id: 2,
         image: BJ,
         title: "Bhoomika Jain",
-        subtitle: "CoStAAn, APOGEE '25"
+        subtitle: "CoStAAn, APOGEE '25",
+        link: '../endpoint'
     },
     {
         id: 3,
         image: BJ,
         title: "Oasis 2024 | #52",
-        subtitle: "Regal Roulette"
+        subtitle: "Regal Roulette",
+        link: '../endpoint'
     },
     {
         id: 4,
         image: BJ,
         title: "Oasis 2023 | #51",
-        subtitle: "Painting Coord, Oasis '24"
+        subtitle: "Painting Coord, Oasis '24",
+        link: '../endpoint'
     },
     {
         id: 5,
         image: BJ,
         title: "Oasis 2022 | #50",
-        subtitle: "Publicity Coord, Oasis '24"
+        subtitle: "Publicity Coord, Oasis '24",
+        link: '../endpoint'
     },
     {
         id: 6,
         image: BJ,
         title: "Oasis 2019 | #49",
-        subtitle: "Media Coord, Oasis '24"
+        subtitle: "Media Coord, Oasis '24",
+        link: '../endpoint'
     },
     {
         id: 7,
         image: BJ,
         title: "Oasis 2018 | #48",
-        subtitle: "Structure Coord, APOGEE '25"
+        subtitle: "Structure Coord, APOGEE '25",
+        link: '../endpoint'
     },
     {
         id: 8,
         image: BJ,
         title: "Oasis 2017 | #47",
-        subtitle: "Painting Coord, APOGEE '25"
+        subtitle: "Painting Coord, APOGEE '25",
+        link: '../endpoint'
     },
     {
         id: 9,
         image: BJ,
         title: "Oasis 2016 | #46",
-        subtitle: "Publicity Coord, APOGEE '25"
+        subtitle: "Publicity Coord, APOGEE '25",
+        link: '../endpoint'
     },
     {
         id: 10,
         image: BJ,
         title: "Oasis 2015 | #45",
-        subtitle: "Media Coord, APOGEE '25"
+        subtitle: "Media Coord, APOGEE '25",
+        link: '../endpoint'
     },
 
 ];
--- src/Components/Content/Slideshow.jsx
+++ src/Components/Content/Slideshow.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import TechStackList2 from '../../All_Lists/TechStackList2';
 
 const Slideshow = () => {
@@ -31,9 +31,11 @@ const Slideshow = () => {
       <div className='grid lg:grid-cols-3 md:grid-cols-3 grid-cols-1 gap-x-12 gap-y-10 mt-10'>
         {TechStackList2.slice(2).map((tech, index) => (
           <div key={index} data-aos={tech.id % 2 === 0 ? 'fade-down' : 'fade-up'} className='flex flex-col items-center'>
-            <div className='text-silver-800 rounded-2xl border-2 border-silver-700 shadow-silver-glow-strong p-0 flex items-center justify-center h-48 w-80 cursor-pointer transform transition-transform duration-300 hover:scale-105 hover:shadow-silver-glow'>
-              <img className='h-full w-full object-cover rounded-2xl' src={tech.image} alt={tech.title} />
-            </div>
+              <Link to={tech.link}>
+              <div className='text-silver-800 rounded-2xl border-2 border-silver-700 shadow-silver-glow-strong p-0 flex items-center justify-center h-48 w-80 cursor-pointer transform transition-transform duration-300 hover:scale-105 hover:shadow-silver-glow'>
+                <img className='h-full w-full object-cover rounded-2xl' src={tech.image} alt={tech.title} />
+              </div>
+              </Link>
             <h2 className='text-[26px] font-semibold mt-4 text-center text-silver-700'>{tech.title}</h2>
           </div>
         ))}
