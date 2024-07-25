2a5d65a6406008efeaf268049d90a45ae29b2a26
Shreyansh vanjani <98659367+shreyvanjani@users.noreply.github.com>
2024-07-25 19:01:58 +0530

    flexible-dimensions

--- src/Components/Content/Projects.jsx
+++ src/Components/Content/Projects.jsx
@@ -19,33 +19,33 @@ function Projects() {
   };
 
   return (
-    <div id='Our Projects' className='bg-custom-light text-black dark:bg-custom-dark dark:text-white p-20 flex flex-col items-center justify-center relative'>
+    <div id='Our Projects' className='bg-custom-light text-black dark:bg-custom-dark dark:text-white p-10 md:p-20 flex flex-col items-center justify-center relative'>
       
-      <h1 data-aos='fade-right' className='text-[52px] font-semibold mb-8 leading-normal uppercase text-silver-700'>
+      <h1 data-aos='fade-right' className='text-[32px] md:text-[52px] font-semibold mb-8 leading-normal uppercase text-silver-700'>
         Our Projects
       </h1>
       
-      <div className='flex flex-row gap-10'>
+      <div className='flex flex-col md:flex-row flex-wrap gap-10 justify-center'>
         {ProjectsList.slice(0, 2).map((project) => (
           <div key={project.id} data-aos={project.id % 2 === 0 ? 'fade-down' : 'fade-up'} className='flex flex-col items-center group'>
             <Link to={`${project.link}?description=${project.description}`}>
-              <div className='relative h-[400px] w-[400px] overflow-hidden rounded-3xl border-2 border-silver-700 shadow-silver-glow-strong group-hover:border-silver-500 transition-all duration-300'>
+              <div className='relative h-[200px] w-[300px] sm:h-[300px] sm:w-[400px] md:h-[400px] md:w-[500px] overflow-hidden rounded-3xl border-2 border-silver-700 shadow-silver-glow-strong group-hover:border-silver-500 transition-all duration-300'>
                 <img className='absolute inset-0 object-cover w-full h-full' src={project.image} alt="project" />
                 <div className='absolute inset-0 border-2 border-silver-500 rounded-3xl opacity-0 group-hover:opacity-100 transition-all duration-300 pointer-events-none shadow-silver-glow'></div>
               </div>
-              <p className='flex gap-2 h-full mt-4 justify-center text-silver-700 text-[36px] text-center cursor-pointer'>
+              <p className='flex gap-2 h-full mt-4 justify-center text-silver-700 text-[24px] md:text-[36px] text-center cursor-pointer'>
                 {project.title}
               </p>
             </Link>
             <div className='flex gap-4 mt-2'>
               <a href={project.instagram} target="_blank" rel="noopener noreferrer">
-                <FaInstagram className='text-silver-700 hover:text-silver-500 transition duration-300' size={24} />
+                <FaInstagram className='text-silver-700 hover:text-silver-500 transition duration-300' size={20} md:size={24} />
               </a>
               <a href={project.linkedin} target="_blank" rel="noopener noreferrer">
-                <FaLinkedin className='text-silver-700 hover:text-silver-500 transition duration-300' size={24} />
+                <FaLinkedin className='text-silver-700 hover:text-silver-500 transition duration-300' size={20} md:size={24} />
               </a>
               <a href={project.twitter} target="_blank" rel="noopener noreferrer">
-                <FaTwitter className='text-silver-700 hover:text-silver-500 transition duration-300' size={24} />
+                <FaTwitter className='text-silver-700 hover:text-silver-500 transition duration-300' size={20} md:size={24} />
               </a>
             </div>
           </div>
@@ -56,4 +56,3 @@ function Projects() {
 }
 
 export default Projects;
-
--- src/Components/Content/TechStack.jsx
+++ src/Components/Content/TechStack.jsx
@@ -37,90 +37,95 @@ function TechStack() {
     const buttonNames = ['Structure', 'Painting', 'Publicity', 'Media'];
 
     return (
-        <div id='Our Team' className='bg-custom-light text-black dark:bg-custom-dark dark:text-white p-20 flex flex-col items-center justify-center'>
-            <h1 data-aos='fade-right' className='text-[72px] font-semibold mb-4 leading-normal uppercase text-silver-700'>Our Team</h1>
-            <p data-aos='fade-left' className='text-lg mb-20 text-center opacity-0 transition-opacity duration-1000 ease-in-out'>
-                Meet the passionate and dedicated members who make up our incredible team.
-            </p>
-
-            <div className='flex flex-col items-center'>
-                <div className='flex flex-row gap-28 mb-10'>
-                    {TechStackList.slice(0, 2).map((tech, index) => (
-                        <div key={index} data-aos={tech.id % 2 === 0 ? 'fade-down' : 'fade-up'} className='flex flex-col items-center'>
-                            <div className='text-silver-800 rounded-3xl border-2 border-silver-700 shadow-silver-glow-strong p-0 flex items-center justify-center h-56 w-56 cursor-pointer transform transition-transform duration-300 hover:scale-105 hover:shadow-silver-glow' onClick={() => handleImageClick(tech.image)}>
-                                <img className='h-full w-full object-cover rounded-3xl' src={tech.image} alt="" />
-                            </div>
-                            <h2 className='text-[32px] font-semibold mt-4 text-center text-silver-700'>{tech.title}</h2>
-                            <h3 className='text-[28px] font-medium text-center text-silver-500'>{tech.subtitle}</h3>
-                        </div>
-                    ))}
+        <div id='Our Team' className='bg-custom-light text-black dark:bg-custom-dark dark:text-white p-10 md:p-20 flex flex-col items-center justify-center'>
+          
+          <h1 data-aos='fade-right' className='text-[32px] md:text-[72px] font-semibold mb-4 leading-normal uppercase text-silver-700'>
+            Our Team
+          </h1>
+          
+          <p data-aos='fade-left' className='text-base md:text-lg mb-20 text-center opacity-0 transition-opacity duration-1000 ease-in-out'>
+            Meet the passionate and dedicated members who make up our incredible team.
+          </p>
+    
+          <div className='flex flex-col items-center w-full'>
+            
+            <div className='flex flex-wrap justify-center gap-10 mb-10'>
+              {TechStackList.slice(0, 2).map((tech, index) => (
+                <div key={index} data-aos={tech.id % 2 === 0 ? 'fade-down' : 'fade-up'} className='flex flex-col items-center'>
+                  <div className='text-silver-800 rounded-3xl border-2 border-silver-700 shadow-silver-glow-strong p-0 flex items-center justify-center h-[200px] w-[200px] sm:h-[250px] sm:w-[250px] md:h-[300px] md:w-[300px] cursor-pointer transform transition-transform duration-300 hover:scale-105 hover:shadow-silver-glow' onClick={() => handleImageClick(tech.image)}>
+                    <img className='h-full w-full object-cover rounded-3xl' src={tech.image} alt="" />
+                  </div>
+                  <h2 className='text-[20px] md:text-[32px] font-semibold mt-4 text-center text-silver-700'>{tech.title}</h2>
+                  <h3 className='text-[18px] md:text-[28px] font-medium text-center text-silver-500'>{tech.subtitle}</h3>
                 </div>
-
-                <div className='grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-10 mb-10'>
-                    {TechStackList.slice(2, 10).map((tech, index) => (
-                        <div key={index} data-aos={tech.id % 2 === 0 ? 'fade-down' : 'fade-up'} className='flex flex-col items-center'>
-                            <div className='text-silver-800 rounded-3xl border-2 border-silver-700 shadow-silver-glow-strong p-0 flex items-center justify-center h-36 w-36 cursor-pointer transform transition-transform duration-300 hover:scale-105 hover:shadow-silver-glow' onClick={() => handleImageClick(tech.image)}>
-                                <img className='h-full w-full object-cover rounded-3xl' src={tech.image} alt="" />
-                            </div>
-                            <h2 className='text-[26px] font-semibold mt-4 text-center text-silver-700'>{tech.title}</h2>
-                            <h3 className='text-[20px] font-medium text-center text-silver-500'>{tech.subtitle}</h3>
-                        </div>
-                    ))}
-                </div>
-
-                <div className='grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-20 mb-10'>
-                    {buttonNames.map((name, index) => (
-                        <div key={index} className='flex flex-col items-center' data-aos='fade-up' data-aos-delay={`${index * 100}`}>
-                            <button
-                                className='bg-dark-custom text-white border border-light-custom p-4 rounded-xl shadow-lg transition-transform duration-300 hover:scale-105 hover:shadow-custom-light-glow font-bold'
-                                style={{ width: '12rem', fontSize: '16px', boxShadow: '0 4px 8px rgba(255, 255, 255, 0.5)' }}
-                                onClick={() => handleButtonClick(index)}
-                            >
-                                {name}
-                            </button>
-                        </div>
-                    ))}
-                </div>
+              ))}
             </div>
-
-            {/* Modal for images */}
-            {selectedImage && (
-                <div className='fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50'>
-                    <div className='relative bg-black rounded-xl overflow-hidden' style={{ width: '45vw', height: '45vw' }}>
-                        <button onClick={handleCloseModal} className='absolute top-4 right-4 text-white text-2xl font-bold hover:text-silver-500'>
-                            &times;
-                        </button>
-                        <img className='w-full h-full object-cover' src={selectedImage} alt="Expanded view" />
-                    </div>
+    
+            <div className='grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-10 mb-10'>
+              {TechStackList.slice(2, 10).map((tech, index) => (
+                <div key={index} data-aos={tech.id % 2 === 0 ? 'fade-down' : 'fade-up'} className='flex flex-col items-center'>
+                  <div className='text-silver-800 rounded-3xl border-2 border-silver-700 shadow-silver-glow-strong p-0 flex items-center justify-center h-[150px] w-[150px] sm:h-[200px] sm:w-[200px] md:h-[250px] md:w-[250px] cursor-pointer transform transition-transform duration-300 hover:scale-105 hover:shadow-silver-glow' onClick={() => handleImageClick(tech.image)}>
+                    <img className='h-full w-full object-cover rounded-3xl' src={tech.image} alt="" />
+                  </div>
+                  <h2 className='text-[20px] md:text-[26px] font-semibold mt-4 text-center text-silver-700'>{tech.title}</h2>
+                  <h3 className='text-[16px] md:text-[20px] font-medium text-center text-silver-500'>{tech.subtitle}</h3>
                 </div>
-            )}
-
-            {selectedItem && (
-                <div className='fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50'>
-                    <div className='relative bg-black rounded-xl overflow-hidden w-full max-w-4xl h-auto max-h-4xl m-4'>
-                        <button onClick={handleCloseModal} className='absolute top-4 right-4 text-white text-2xl font-bold hover:text-silver-500'>
-                            &times;
-                        </button>
-                        <div className='flex flex-col md:flex-row h-full'>
-                            <div className='w-full md:w-1/2 h-64 md:h-auto'>
-                                <img className='w-full h-full object-cover' src={selectedItem.picture} alt="Item" />
-                            </div>
-                            <div className='w-full md:w-1/2 p-4 overflow-y-auto'>
-                                <h2 className='text-3xl font-bold text-white mb-2'>{selectedItem.title}</h2>
-                                <p className='text-white mb-4'>{selectedItem.description}</p>
-                                <ul className='list-disc list-inside text-white'>
-                                    {selectedItem.list.map((name, index) => (
-                                        <li key={index}>{name}</li>
-                                    ))}
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
+              ))}
+            </div>
+    
+            <div className='grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-10 mb-10'>
+              {buttonNames.map((name, index) => (
+                <div key={index} className='flex flex-col items-center' data-aos='fade-up' data-aos-delay={`${index * 100}`}>
+                  <button
+                    className='bg-dark-custom text-white border border-light-custom p-4 rounded-xl shadow-lg transition-transform duration-300 hover:scale-105 hover:shadow-custom-light-glow font-bold'
+                    style={{ width: '12rem', fontSize: '16px', boxShadow: '0 4px 8px rgba(255, 255, 255, 0.5)' }}
+                    onClick={() => handleButtonClick(index)}
+                  >
+                    {name}
+                  </button>
                 </div>
-            )}
+              ))}
+            </div>
+          </div>
+    
+          {/* Modal for images */}
+          {selectedImage && (
+            <div className='fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50'>
+              <div className='relative bg-black rounded-xl overflow-hidden w-full max-w-3xl h-auto max-h-3xl'>
+                <button onClick={handleCloseModal} className='absolute top-4 right-4 text-white text-2xl font-bold hover:text-silver-500'>
+                  &times;
+                </button>
+                <img className='w-full h-full object-cover' src={selectedImage} alt="Expanded view" />
+              </div>
+            </div>
+          )}
+    
+          {selectedItem && (
+            <div className='fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50'>
+              <div className='relative bg-black rounded-xl overflow-hidden w-full max-w-4xl h-auto max-h-4xl'>
+                <button onClick={handleCloseModal} className='absolute top-4 right-4 text-white text-2xl font-bold hover:text-silver-500'>
+                  &times;
+                </button>
+                <div className='flex flex-col md:flex-row h-full'>
+                  <div className='w-full md:w-1/2 h-64 md:h-auto'>
+                    <img className='w-full h-full object-cover' src={selectedItem.picture} alt="Item" />
+                  </div>
+                  <div className='w-full md:w-1/2 p-4 overflow-y-auto'>
+                    <h2 className='text-3xl font-bold text-white mb-2'>{selectedItem.title}</h2>
+                    <p className='text-white mb-4'>{selectedItem.description}</p>
+                    <ul className='list-disc list-inside text-white'>
+                      {selectedItem.list.map((name, index) => (
+                        <li key={index}>{name}</li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
-    );
-}
+      );
+    }
 
 export default TechStack;
 
