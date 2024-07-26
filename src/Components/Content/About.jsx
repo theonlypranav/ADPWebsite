0438b218a1392fd525ab35f402c5a5a8d69543e2
Krish-hashhh <f20230887@pilani.bits-pilani.ac.in>
2024-07-27 00:48:51 +0530

    responsive

--- src/Components/Content/About.jsx
+++ src/Components/Content/About.jsx
@@ -39,10 +39,10 @@ function About() {
         onClick={openModal}
       />
       <div className='h-full lg:py-40 flex flex-col justify-center lg:items-start items-center'>
-        <h1 data-aos='fade-right' className='text-[32px] sm:text-[40px] md:text-[48px] lg:text-[52px] font-semibold mb-8 leading-normal text-silver-700 uppercase'>
+        <h1 data-aos='fade-right' className='text-[24px] sm:text-[28px] md:text-[32px] lg:text-[36px] font-semibold mb-8 leading-normal text-silver-700 uppercase'>
           About ADP
         </h1>
-        <p data-aos='fade-left' className='text-sm sm:text-base md:text-lg lg:text-xl'>
+        <p data-aos='fade-left' className='text-[14px] sm:text-[16px] md:text-[18px] lg:text-[20px] xl:text-[22px] leading-relaxed text-silver-700'>
           Who are we, you ask? We're the Department of Art, Design and Publicity.
           <br /><br />
           We are responsible for publicising our festivals: Oasis and APOGEE with promotional material, and adorning the campus with massive paintings and breathtaking structures alike.
--- src/Components/Content/Home.jsx
+++ src/Components/Content/Home.jsx
@@ -37,10 +37,10 @@ function Home() {
   return (
     <div id='Home' className='bg-custom-light text-black dark:bg-custom-dark dark:text-white lg:px-56 lg:py-0 px-10 py-20 text-center gap-5 lg:text-start flex lg:flex-row flex-col-reverse justify-between lg:gap-28 items-center min-h-screen overflow-hidden'>
       <div className='h-full lg:py-40 flex flex-col justify-center lg:items-start items-center'>
-        <h1 data-aos='fade-right' style={{ fontFamily: 'Anton',letterSpacing:0.8 }} className='text-[14px] sm:text-[18px] md:text-[24px] lg:text-[30px] font-bold mb-8 leading-normal uppercase'>
-         Welcome To The Department of<br /><span className='text-[32px] sm:text-[40px] md:text-[56px] lg:text-[72px] font-bold mb-8 leading-normal uppercase text-silver-700'> Art, Design and Publicity</span>
+        <h1 data-aos='fade-right' style={{ fontFamily: 'Anton', letterSpacing: 0.8 }} className='text-[14px] sm:text-[18px] md:text-[24px] lg:text-[30px] font-bold mb-8 leading-normal uppercase'>
+          Welcome To The Department of<br /><span className='text-[32px] sm:text-[40px] md:text-[56px] lg:text-[72px] font-bold mb-8 leading-normal uppercase text-silver-700'> Art, Design and Publicity</span>
         </h1>
-        <p data-aos='fade-left' style={{ fontFamily: 'Poppins',letterSpacing:0.8 }}className='text-[32px] sm:text-[40px] md:text-[56px] lg:text-[18px] font- mb-8 leading-normal  text-silver-700'>
+        <p data-aos='fade-left' style={{ fontFamily: 'Poppins', letterSpacing: 0.8 }} className='text-[14px] sm:text-[16px] md:text-[18px] lg:text-[20px] xl:text-[22px]  mb-8 leading-normal text-silver-700'>
           A collective of passionate artists, we are the visionaries behind the stunning aesthetics of Oasis and APOGEE, the grand cultural and technical fest of BITS Pilani. From breathtaking backdrops to mind-blowing structures, we craft the ambiance that defines these iconic festivals. We are the architects of the visual narrative, infusing every corner with creativity and innovation. Beyond the artistic realm, we are the driving force behind the publicity of these flagship events. Our canvas is the stage, and our colors are imagination. We create more than just visuals; we craft experiences.
         </p>
         <div data-aos='fade-up' className='flex mt-8 gap-2'>
@@ -57,7 +57,7 @@ function Home() {
           </div>
         </div>
       </div>
-      <img data-aos='fade-up' src={img} alt="ADP Logo" className='rounded-full border-2 p-1 border-silver-700 img_glow sm:w-[300px] sm:h-[300px] md:w-[400px] md:h-[400px] lg:w-[500px] lg:h-[500px]' />
+      <img data-aos='fade-up' src={img} alt="ADP Logo" className='rounded-full border-2 p-1 border-silver-700 img_glow w-[200px] sm:w-[300px] sm:h-[300px] md:w-[400px] md:h-[400px] lg:w-[500px] lg:h-[500px]' />
     </div>
   );
 }
