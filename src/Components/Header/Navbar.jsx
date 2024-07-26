d12b1ac8e6d67f121b50b1a60204a9cd725f37c8
YoursAJ24 <148056802+YoursAJ24@users.noreply.github.com>
2024-07-27 01:38:04 +0530

    merged stuccans and costaans buttons into our legacy

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link as ScrollLink } from 'react-scroll';
 import { Link as RouterLink, useLocation } from 'react-router-dom';
-import light from '../../assets/light.png';
-import dark from '../../assets/dark.png';
 import logo from '../../assets/ADPLogoNewGrad.png';
 import { FaTimes } from 'react-icons/fa';
 import { CiMenuFries } from 'react-icons/ci';
@@ -41,7 +39,7 @@ function Navbar() {
 
   return (
     <>
-      <nav className='fixed top-0 left-0 right-0 bg-black text-white z-50 overflow-hidden'>
+      <nav className='fixed top-0 left-0 right-0 bg-black text-white z-50'>
         <div className='flex items-center justify-between px-4 py-4 lg:px-20 lg:py-6 border-b border-silver-500'>
           <RouterLink to='/' className='flex items-center'>
             <img src={logo} alt="Logo" className='h-8 w-8 lg:h-12 lg:w-12' />
@@ -52,7 +50,7 @@ function Navbar() {
           >
             {click ? <FaTimes /> : <CiMenuFries />}
           </button>
-          <div className='hidden lg:flex flex-1 items-center justify-end'>
+          <div className='hidden lg:flex flex-1 items-center justify-end relative'>
             <ul className='flex gap-8 text-base items-center'>
               {navItems.map((item, index) => (
                 isHomePage ? (
@@ -79,16 +77,23 @@ function Navbar() {
                   </RouterLink>
                 )
               ))}
-              <RouterLink to='/treedesign?param=stuccan' >
-                <li className='border-b-0 hover:border-b-2 hover:border-silver-700 hover:text-silver-700 transition cursor-pointer whitespace-nowrap'>
-                  StuCCAn's
-                </li>
-              </RouterLink>
-              <RouterLink to='/treedesign?param=costaan' >
-                <li className='border-b-0 hover:border-b-2 hover:border-silver-700 hover:text-silver-700 transition cursor-pointer whitespace-nowrap'>
-                  CoStAAn's
-                </li>
-              </RouterLink>
+              <div className='relative group'>
+                <button className='border-b-0 hover:border-b-2 hover:border-silver-700 hover:text-silver-700 transition cursor-pointer whitespace-nowrap'>
+                  Our Legacy
+                </button>
+                <div className='absolute left-0 mt-2 bg-black text-white border border-silver-500 py-2 px-3 rounded-lg opacity-0 group-hover:opacity-100 group-hover:visible transition-opacity z-50'>
+                  <RouterLink to='/treedesign?param=stuccan'>
+                    <li className='p-2 hover:border-b-2 hover:border-silver-700 hover:text-silver-700 cursor-pointer whitespace-nowrap rounded-t-lg'>
+                      StuCCAn's
+                    </li>
+                  </RouterLink>
+                  <RouterLink to='/treedesign?param=costaan'>
+                    <li className='p-2 hover:border-b-2 hover:border-silver-700 hover:text-silver-700 cursor-pointer whitespace-nowrap rounded-b-lg'>
+                      CoStAAn's
+                    </li>
+                  </RouterLink>
+                </div>
+              </div>
             </ul>
           </div>
         </div>
@@ -122,16 +127,23 @@ function Navbar() {
                   </RouterLink>
                 )
               ))}
-              <RouterLink to='/treedesign?param=stuccan' onClick={() => setClick(false)}>
-                <li className='border-b-0 hover:border-b-2 hover:border-silver-700 hover:text-silver-700 transition cursor-pointer whitespace-nowrap'>
-                  StuCCAn's
-                </li>
-              </RouterLink>
-              <RouterLink to='/treedesign?param=costaan' onClick={() => setClick(false)}>
-                <li className='border-b-0 hover:border-b-2 hover:border-silver-700 hover:text-silver-700 transition cursor-pointer whitespace-nowrap'>
-                  CoStAAn's
-                </li>
-              </RouterLink>
+              <div className='relative group'>
+                <button className='border-b-0 hover:border-b-2 hover:border-silver-700 hover:text-silver-700 transition cursor-pointer whitespace-nowrap'>
+                  Our Legacy
+                </button>
+                <div className='absolute left-0 mt-2 bg-black text-white border border-silver-500 py-2 px-3 rounded-lg opacity-0 group-hover:opacity-100 group-hover:visible transition-opacity z-50'>
+                  <RouterLink to='/treedesign?param=stuccan'>
+                    <li className='p-2 hover:border-b-2 hover:border-silver-700 hover:text-silver-700 cursor-pointer whitespace-nowrap rounded-t-lg'>
+                      StuCCAn's
+                    </li>
+                  </RouterLink>
+                  <RouterLink to='/treedesign?param=costaan'>
+                    <li className='p-2 hover:border-b-2 hover:border-silver-700 hover:text-silver-700 cursor-pointer whitespace-nowrap rounded-b-lg'>
+                      CoStAAn's
+                    </li>
+                  </RouterLink>
+                </div>
+              </div>
             </ul>
           </div>
         )}
