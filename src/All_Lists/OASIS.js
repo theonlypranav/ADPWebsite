68ca089448fec75fbecbf9497c27873b8117cca0
Krish-hashhh <f20230887@pilani.bits-pilani.ac.in>
2024-07-27 03:33:16 +0530

    oasis updated

--- src/All_Lists/OASIS.js
+++ src/All_Lists/OASIS.js
@@ -6,8 +6,17 @@ const TechStackList2 = [
     {
         id: 1,
         image: BJ,
+        title: "Oasis 2024 | #52",
+        subtitle: "Regalle Roulette",
+        link:"/endpoint",
+        description: "OASIS 2024"
+    },
+
+    {
+        id: 1,
+        image: BJ,
         title: "Oasis 2023 | #51",
-        subtitle: "Regal Roulette",
+        subtitle: "Grimoire Galore",
         link: "/endpoint",
         description:"OASIS 2023"
     },
@@ -15,10 +24,199 @@ const TechStackList2 = [
         id: 2,
         image: BJ,
         title: "Oasis 2022 | #50",
-        subtitle: "Grimoire Galore",
+        subtitle: "Demenesce of the Lost Gold",
         link:"/endpoint",
         description: "OASIS 2022"
-    }
+    },
+    {
+        id: 3,
+        image: BJ,
+        title: "Oasis 2020 | #49",
+        subtitle: "The Alchemist's Prophecy",
+        link:"/endpoint",
+        description: "OASIS 2020"
+    },
+    {
+        id: 4,
+        image: BJ,
+        title: "Oasis 2019 | #48",
+        subtitle: "Neon-Noir",
+        link:"/endpoint",
+        description: "OASIS 2019"
+    },
+    {
+        id: 5,
+        image: BJ,
+        title: "Oasis 2018 | #47",
+        subtitle: "The Far Out Fest",
+        link:"/endpoint",
+        description: "OASIS 2018"
+    },
+    {
+        id: 6,
+        image: BJ,
+        title: "Oasis 2017 | #46",
+        subtitle: "Ye toh pata hi nhi",
+        link:"/endpoint",
+        description: "OASIS 2017"
+    },
+    {
+        id: 7,
+        image: BJ,
+        title: "Oasis 2016 | #45",
+        subtitle: "Ye toh pata hi nhi",
+        link:"/endpoint",
+        description: "OASIS 2016"
+    },
+    {
+        id: 8,
+        image: BJ,
+        title: "Oasis 2015 | #44",
+        subtitle: "Ye toh pata hi nhi",
+        link:"/endpoint",
+        description: "OASIS 2015"
+    },
+    {
+        id: 9,
+        image: BJ,
+        title: "Oasis 2014 | #43",
+        subtitle: "Ye toh pata hi nhi",
+        link:"/endpoint",
+        description: "OASIS 2014"
+    },
+    {
+        id: 10,
+        image: BJ,
+        title: "Oasis 2013 | #42",
+        subtitle: "Ye toh pata hi nhi",
+        link:"/endpoint",
+        description: "OASIS 2013"
+    },
+    {
+        id: 11,
+        image: BJ,
+        title: "Oasis 2012 | #41",
+        subtitle: "Ye toh pata hi nhi",
+        link:"/endpoint",
+        description: "OASIS 2012"
+    },
+    {
+        id: 12,
+        image: BJ,
+        title: "Oasis 2011 | #40",
+        subtitle: "Ye toh pata hi nhi",
+        link:"/endpoint",
+        description: "OASIS 2011"
+    },
+    {
+        id: 13,
+        image: BJ,
+        title: "Oasis 2010 | #39",
+        subtitle: "Insomnia",
+        link:"/endpoint",
+        description: "OASIS 2010"
+    },
+    {
+        id: 14,
+        image: BJ,
+        title: "Oasis 2009 | #38",
+        subtitle: "Ye toh pata hi nhi",
+        link:"/endpoint",
+        description: "OASIS 2009"
+    },
+    {
+        id: 15,
+        image: BJ,
+        title: "Oasis 2008 | #37",
+        subtitle: "Unio Mystica",
+        link:"/endpoint",
+        description: "OASIS 2008"
+    },
+    {
+        id: 16,
+        image: BJ,
+        title: "Oasis 2007 | #36",
+        subtitle: "Psychedelia",
+        link:"/endpoint",
+        description: "OASIS 2007"
+    },
+    {
+        id: 17,
+        image: BJ,
+        title: "Oasis 2006 | #35",
+        subtitle: "Scintillations",
+        link:"/endpoint",
+        description: "OASIS 2006"
+    },
+    {
+        id: 18,
+        image: BJ,
+        title: "Oasis 2005 | #34",
+        subtitle: "Nix Periphera",
+        link:"/endpoint",
+        description: "OASIS 2005"
+    },
+    {
+        id: 19,
+        image: BJ,
+        title: "Oasis 2004 | #33",
+        subtitle: "Expressions Unleashed",
+        link:"/endpoint",
+        description: "OASIS 2004"
+    },
+    {
+        id: 20,
+        image: BJ,
+        title: "Oasis 2003 | #32",
+        subtitle: "My Elysium",
+        link:"/endpoint",
+        description: "OASIS 2003"
+    },
+    {
+        id: 21,
+        image: BJ,
+        title: "Oasis 2002 | #31",
+        subtitle: "Apotheosis",
+        link:"/endpoint",
+        description: "OASIS 2002"
+    },
+    {
+        id: 22,
+        image: BJ,
+        title: "Oasis 2001 | #30",
+        subtitle: "Praharsha",
+        link:"/endpoint",
+        description: "OASIS 2001"
+    },
+    {
+        id: 23,
+        image: BJ,
+        title: "Oasis 2000 | #29",
+        subtitle: "Adventure",
+        link:"/endpoint",
+        description: "OASIS 2000"
+    },
+    {
+        id: 24,
+        image: BJ,
+        title: "Oasis 1999 | #28",
+        subtitle: "Mystic Realms",
+        link:"/endpoint",
+        description: "OASIS 1999"
+    },
+    {
+        id: 25,
+        image: BJ,
+        title: "Oasis 1998 | #27",
+        subtitle: "Call of The Wild",
+        link:"/endpoint",
+        description: "OASIS 1998"
+    },
+   
+
+
+
+
 
 ];
 
