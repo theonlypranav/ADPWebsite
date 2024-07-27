457bacdbeb8d29025ef1d568cabe2d2178b12b24
Krish-hashhh <f20230887@pilani.bits-pilani.ac.in>
2024-07-27 23:19:04 +0530

    responsive

--- src/Components/Content/About.jsx
+++ src/Components/Content/About.jsx
@@ -41,11 +41,10 @@ function About() {
         onClick={openModal}
       />
       <div className='h-full lg:py-20 flex flex-col justify-center lg:items-start items-center'>
-        <h1 data-aos='fade-right' style={{fontFamily:'Anton', letterSpacing:0.8 }}className='text-[32px] sm:text-[40px] md:text-[48px] lg:text-[60px] font-semibold mb-8 leading-normal text-silver-700 uppercase'>
+        <h1 data-aos='fade-right' style={{ fontFamily: 'Anton', letterSpacing: 0.8 }} className='text-[32px] sm:text-[40px] md:text-[48px] lg:text-[60px] font-semibold mb-8 leading-normal text-silver-700 uppercase'>
           About ADP
         </h1>
-        <p data-aos='fade-left'  style={{ fontFamily: 'Poppins', letterSpacing: 0.8 }}
-            className="text-[14px] sm:text-[16px] md:text-[18px] lg:text-[20px] xl:text-[22px] mb-8 leading-normal text-silver-700 max-w-[500px] lg:max-w-none">
+        <p data-aos='fade-left' style={{ fontFamily: 'Poppins', letterSpacing: 0.8 }} className="text-[14px] sm:text-[16px] md:text-[18px] lg:text-[20px] xl:text-[22px] mb-8 leading-normal text-silver-700 max-w-[500px] lg:max-w-none">
           Who are we, you ask? We're the Department of Art, Design and Publicity.
           <br /><br />
           We are responsible for publicising our festivals: Oasis and APOGEE with promotional material, and adorning the campus with massive paintings and breathtaking structures alike.
@@ -60,7 +59,7 @@ function About() {
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
         contentLabel="Image Modal"
-        className='flex justify-center items-center'
+        className='modal-content flex justify-center items-center'
         overlayClassName='fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center'
       >
         <div className='relative bg-white dark:bg-custom-dark rounded-lg shadow-lg p-4 w-full max-w-[90%] max-h-[90%] flex flex-col items-center'>
