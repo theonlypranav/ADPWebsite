19c3f6b7967e34bdd83ece667907b94e9446cdf8
YoursAJ24 <148056802+YoursAJ24@users.noreply.github.com>
2024-07-27 23:33:06 +0530

    corrected our legacy

--- src/All_Lists/oasisheads.js
+++ src/All_Lists/oasisheads.js
@@ -1,76 +1,27 @@
-import HTML from '../assets/Cords/SV2.png';
-import BJ from '../assets/Cords/bj.jpeg';
-import ND from '../assets/Cords/ND.png';
-import REACT from '../assets/Cords/PD.png';
-import TAILWIND from '../assets/Cords/Aj.png';
-import REDUX from '../assets/Cords/Sj.jpeg';
-import GRADIOCJ from '../assets/Cords/CJ.png';
-import GRADIOAP from '../assets/Cords/AP.png';
-import GRADIOSH from '../assets/Cords/SH.png';
-import GRADIOSM from '../assets/Cords/SM.png';
+import i1 from '../assets/stuccans/shreyansh vanjani.png';
+import i2 from '../assets/stuccans/adarsh goel.jpg';
+import i3 from '../assets/stuccans/rhythm saxena.jpg';
+
 
 const oasisheads = [
     {
         id: 1,
-        image: HTML,
+        image: i1,
         title: "Shreyansh Vanjani",
         subtitle: "StuCCAn, Oasis '24"
     },
     {
         id: 2,
-        image: BJ,
-        title: "Bhoomika Jain",
-        subtitle: "CoStAAn, APOGEE '25"
+        image: i2,
+        title: "Adarsh Goel",
+        subtitle: "StuCCAn, Oasis '23"
     },
     {
-        id: 3,
-        image: TAILWIND,
-        title: "Ayush Jain",
-        subtitle: "Structure Coord, Oasis '24"
+        id: 1,
+        image: i3,
+        title: "Rhythm Saxena",
+        subtitle: "StuCCAn, Oasis '22"
     },
-    {
-        id: 4,
-        image: ND,
-        title: "Navya Dhiman",
-        subtitle: "Painting Coord, Oasis '24"
-    },
-    {
-        id: 5,
-        image: REACT,
-        title: "Pranav Deshpande",
-        subtitle: "Publicity Coord, Oasis '24"
-    },
-    {
-        id: 6,
-        image: REDUX,
-        title: "Sarthak Juneja",
-        subtitle: "Media Coord, Oasis '24"
-    },
-    {
-        id: 7,
-        image: GRADIOSH,
-        title: "Shlok Jaiswal",
-        subtitle: "Structure Coord, APOGEE '25"
-    },
-    {
-        id: 8,
-        image: GRADIOSM,
-        title: "Saanidhya Mittal",
-        subtitle: "Painting Coord, APOGEE '25"
-    },
-    {
-        id: 9,
-        image: GRADIOCJ,
-        title: "Chavi Gupta",
-        subtitle: "Publicity Coord, APOGEE '25"
-    },
-    {
-        id: 10,
-        image: GRADIOAP,
-        title: "Archi Patel",
-        subtitle: "Media Coord, APOGEE '25"
-    },
-
 ];
 
 export default oasisheads;
