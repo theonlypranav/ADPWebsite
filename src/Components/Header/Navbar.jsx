2ce9e3097b5e4cb27f1432b71f0e5bbff5d56bbe
Krish-hashhh <f20230887@pilani.bits-pilani.ac.in>
2024-07-28 00:20:07 +0530

    navbar

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -33,7 +33,7 @@ function Navbar() {
 
   const handleClick = () => setClick(!click);
 
-  const navItems = ['Home', 'About Us', 'Our Projects', 'Our Team'];
+  const navItems = ['Home', 'AboutUs', 'OurProjects', 'OurTeam'];
 
   const isHomePage = location.pathname === '/';
 
