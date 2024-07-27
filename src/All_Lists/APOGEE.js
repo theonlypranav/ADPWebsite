cd831008808f0ee3a5ff15b60735a6d1663563d3
navya303 <f20230192@pilani.bits-pilani.ac.in>
2024-07-27 16:03:11 +0530

    Update APOGEE.js

--- src/All_Lists/APOGEE.js
+++ src/All_Lists/APOGEE.js
@@ -7,7 +7,7 @@ const TechStackList2 = [
         id: 1,
         image: BJ,
         title: "APOGEE 2024",
-        subtitle: "Jo bhoomika decide kare",
+        subtitle: "To be decided",
         link:"/endpoint",
         description: "APOGEE 2024"
     },
@@ -15,9 +15,193 @@ const TechStackList2 = [
         id: 2,
         image: BJ,
         title: "APOGEE 2023",
-        subtitle: "A Hivemind Genesis",
+        subtitle: "Celestial Epiphany",
         link:"/endpoint",
         description: "APOGEE 2023"
+    },
+    {
+        id: 3,
+        image: BJ,
+        title: "APOGEE 2022",
+        subtitle: "A Hivemind Genesis",
+        link:"/endpoint",
+        description: "APOGEE 2022"
+    },
+    {
+        id: 4,
+        image: BJ,
+        title: "APOGEE 2021",
+        subtitle: "Encrypted Dimension",
+        link:"/endpoint",
+        description: "APOGEE 2021"
+    },
+    {
+        id: 5,
+        image: BJ,
+        title: "APOGEE 2020",
+        subtitle: "Digitilized Renaissance",
+        link:"/endpoint",
+        description: "APOGEE 2020"
+    },
+    {
+        id: 6,
+        image: BJ,
+        title: "APOGEE 2019",
+        subtitle: "The Glitch Repository",
+        link:"/endpoint",
+        description: "APOGEE 2019"
+    },
+    {
+        id: 7,
+        image: BJ,
+        title: "APOGEE 2018",
+        subtitle: "Reality Roulette",
+        link:"/endpoint",
+        description: "APOGEE 2018"
+    },
+    {
+        id: 8,
+        image: BJ,
+        title: "APOGEE 2017",
+        subtitle: "A Cybernetic Vision",
+        link:"/endpoint",
+        description: "APOGEE 2017"
+    },
+    {
+        id: 9,
+        image: BJ,
+        title: "APOGEE 2016",
+        subtitle: "Ihe Retro Vision",
+        link:"/endpoint",
+        description: "APOGEE 2016"
+    },
+    {
+        id: 10,
+        image: BJ,
+        title: "APOGEE 2015",
+        subtitle: "Information Unavailable",
+        link:"/endpoint",
+        description: "APOGEE 2015"
+    },
+    {
+        id: 11,
+        image: BJ,
+        title: "APOGEE 2014",
+        subtitle: "Information Unavailable",
+        link:"/endpoint",
+        description: "APOGEE 2014"
+    },
+    {
+        id: 12,
+        image: BJ,
+        title: "APOGEE 2013",
+        subtitle: "Information Unavailable",
+        link:"/endpoint",
+        description: "APOGEE 2013"
+    },
+    {
+        id: 13,
+        image: BJ,
+        title: "APOGEE 2012",
+        subtitle: "Information Unavailable",
+        link:"/endpoint",
+        description: "APOGEE 2012"
+    },
+    {
+        id: 14,
+        image: BJ,
+        title: "APOGEE 2011",
+        subtitle: "Information Unavailable",
+        link:"/endpoint",
+        description: "APOGEE 2011"
+    },
+    {
+        id: 15,
+        image: BJ,
+        title: "APOGEE 2010",
+        subtitle: "Information Unavailable",
+        link:"/endpoint",
+        description: "APOGEE 2010"
+    },
+    {
+        id: 16,
+        image: BJ,
+        title: "APOGEE 2009",
+        subtitle: "Information Unavailable",
+        link:"/endpoint",
+        description: "APOGEE 2009"
+    },
+    {
+        id: 17,
+        image: BJ,
+        title: "APOGEE 2008",
+        subtitle: "Thought Pivot",
+        link:"/endpoint",
+        description: "APOGEE 2008"
+    },
+    {
+        id: 18,
+        image: BJ,
+        title: "APOGEE 2007",
+        subtitle: "Dhiti",
+        link:"/endpoint",
+        description: "APOGEE 2007"
+    },
+    {
+        id: 19,
+        image: BJ,
+        title: "APOGEE 2006",
+        subtitle: "Sanshodhanam",
+        link:"/endpoint",
+        description: "APOGEE 2006"
+    },
+    {
+        id: 20,
+        image: BJ,
+        title: "APOGEE 2005",
+        subtitle: "Inspiring Innovation",
+        link:"/endpoint",
+        description: "APOGEE 2005"
+    },
+    {
+        id: 21,
+        image: BJ,
+        title: "APOGEE 2004",
+        subtitle: "Celebrating Technology",
+        link:"/endpoint",
+        description: "APOGEE 2004"
+    },
+    {
+        id: 22,
+        image: BJ,
+        title: "APOGEE 2003",
+        subtitle: "Vivista",
+        link:"/endpoint",
+        description: "APOGEE 2003"
+    },
+    {
+        id: 23,
+        image: BJ,
+        title: "APOGEE 2002",
+        subtitle: "Transcending Reality",
+        link:"/endpoint",
+        description: "APOGEE 2002"
+    },
+    {
+        id: 24,
+        image: BJ,
+        title: "APOGEE 2001",
+        subtitle: "Cradling Aspirations",
+        link:"/endpoint",
+        description: "APOGEE 2001"
+    },
+    {
+        id: 25,
+        image: BJ,
+        title: "APOGEE 2000",
+        subtitle: "Exploring the Genie'us",
+        link:"/endpoint",
+        description: "APOGEE 2000"
     }
 
 ];
