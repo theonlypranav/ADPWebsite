f098c6ded6bd3969727fbff3ea1abe229c259590
neo-astra <93836543+neo-astra@users.noreply.github.com>
2024-07-27 23:16:56 +0530

    list changed

--- src/All_Lists/TeamList.js
+++ src/All_Lists/TeamList.js
@@ -9,25 +9,25 @@ const TeamList = {
       title: 'Structure',
       picture: TAILWIND,
       description: 'If you love hands-on work and visualizing life-size models, this is the place for you! The structure team creates eye-catching 3D models of beloved fictional characters, like Aquaman in the foyer, made during Oasis 22. We use materials like wood, metal rods, and newspapers, and always welcome new ideas and concepts for materials.',
-      list: ['Mann Chhapolia', 'Krish Vora', 'Ayush Jain', 'Sarthak Juneja', 'Priyanshu Sinha', 'Manan Gupta', 'Archi Patel', 'Shlok Jaiswal', 'Aryan Agarwal', 'Samridhhi Sharma', 'Hemanshi Saini', 'Sai Vamsi Addepalli', 'Shelly Paptan']
+      list: ['Ayush Jain','Shlok Jaiswal', 'Sarthak Juneja', 'Archi Patel',  'Aryan Agarwal', 'Samridhhi Sharma','Mann Chhapolia', 'Krish Vora', 'Priyanshu Sinha', 'Manan Gupta', 'Hemanshi Saini', 'Sai Vamsi Addepalli', 'Shelly Paptan']
   },
   1: {
       title: 'Painting',
       picture: ND,
       description: 'If you were a budding artist in school and want to continue your artistic journey in college, this is the place to be! Join the most talented artists on campus to create massive panels displayed in and around the Auditorium during the fests, including the Backdrop and four inside-Auditorium panels. Experiment with various mediums like poster colors, charcoal, and soft pastels.',
-      list: ['Navya Dhiman', 'Rakshita Dave', 'Bhoomika Jain', 'Saanidhya Mittal', 'Harsh Jha', 'Ria Munshi', 'Shruti Adepu', 'Swarnima Bist', 'Devansh Agarwal', 'Kaustubh Tanwar', 'Lakshit Bansal', 'Navya Jain', 'Urvi Singh', 'Rudr Bisht', 'Kamya Saraogi', 'Siya Bansal']
+      list: ['Navya Dhiman', 'Saanidhya Mittal', 'Rakshita Dave', 'Bhoomika Jain', 'Harsh Jha', 'Ria Munshi', 'Shruti Adepu', 'Swarnima Bist', 'Devansh Agarwal', 'Kaustubh Tanwar', 'Lakshit Bansal', 'Navya Jain', 'Urvi Singh', 'Rudr Bisht', 'Kamya Saraogi', 'Siya Bansal']
   },
   2: {
       title: 'Publicity',
       picture: REACT,
       description: 'If you have a knack for graphic design and video editing, and want to enhance the fests digital aesthetics, this is the place for you! The Publicity team manages the social media for APOGEE and Oasis, designing banners, posters, merchandise and brochures. From the Mohit Chauhan poster on the APOGEE page to on-campus standees, we handle the overall branding, short promotional videos, and the theme series for the fest.',
-      list: ['Pranav Deshpande', 'Arpit Maity', 'Sajal Batra', 'Krishnansh Sood', 'Chavi Gupta', 'Unnati Goyal', 'Aurvind Mohanty', 'Aditya Bhusari', 'Harsh Munje', 'Aditya Verma', 'Manan Jain', 'Nishkarsh Jain', 'Saaransh Jain', 'Medhansh Adlakha', 'Nakshatra Garg']
+      list: ['Pranav Deshpande', 'Chavi Gupta', 'Aditya Verma', 'Manan Jain', 'Nishkarsh Jain', 'Saaransh Jain', 'Arpit Maity', 'Sajal Batra', 'Krishnansh Sood', 'Unnati Goyal', 'Aurvind Mohanty', 'Aditya Bhusari', 'Harsh Munje', 'Medhansh Adlakha', 'Nakshatra Garg']
   },
   3: {
       title: 'Media',
       picture: REDUX,
       description: 'In the Media Relations Team, you will be working towards forging collaborations with news media houses, influencers and more for amplifying the reach of the fest.In media relations, you learn to create marketing opportunities for the fests and handle partnerships with various firms, and personalities. In the past, we have collaborated with various companies, organisations like Yahoo, Times of India, Hindustan Times, Red FM, New Delhi Times, News18, Money Control; we play a crucial role in increasing the footfall and spreading the fests name.',
-      list: ['Sarthak Juneja', 'Archi Patel', 'Bhoomika Jain', 'Saanidhya Mittal','Ayush Jain', 'Krish Vora', 'Siya Bansal', 'Swarnima Bist', 'Mann Chhapolia', 'Sai Vamsi Addepalli', 'Lakshit Bansal', 'Krishnansh Sood', 'Devansh Agarwal', 'Aditya Bhusari', 'Rudr Bisht', 'Medhansh Adlakha']
+      list: ['Sarthak Juneja', 'Archi Patel', 'Pranav Deshpande', 'Bhoomika Jain', 'Saanidhya Mittal', 'Ayush Jain', 'Krish Vora', 'Siya Bansal', 'Swarnima Bist', 'Mann Chhapolia', 'Sai Vamsi Addepalli', 'Lakshit Bansal', 'Krishnansh Sood', 'Devansh Agarwal', 'Aditya Bhusari', 'Rudr Bisht', 'Medhansh Adlakha']
   }
 };
 
