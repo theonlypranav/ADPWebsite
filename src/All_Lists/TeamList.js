b6640d9afa41049c624d1d94a6dad3ac253ccdc0
Prasham884 <f20240884@pilani.bits-pilani.ac.in>
2025-07-14 15:28:21 +0530

    Workforce List and Coord photos updated

--- src/All_Lists/TeamList.js
+++ src/All_Lists/TeamList.js
@@ -9,25 +9,25 @@ const TeamList = {
       title: 'Structure',
       picture: TAILWIND,
       description: 'Structure, often referred to as the \'Muscle\' of ADP, deals with creating life-size (or even bigger) models with different elements like wood, metal rods, paper mache and much more. Our creations include one major structure (outside the main Audi), one front structure (in front of the Clock Tower), a secondary structure (outside the FD-2 building) and multiple photo-booths in different key spots on campus. In recent creations, welding has taken utmost prominence with there being significant improvements in the overall structural integrity.',
-      list: ['Ayush Jain','Shlok Jaiswal', 'Sarthak Juneja', 'Archi Patel',  'Aryan Agarwal', 'Samriddhi Sharma','Mann Chhapolia', 'Krish Vora', 'Priyanshu Sinha', 'Manan Gupta', 'Hemanshi Saini', 'Sai Vamsi Addepalli', 'Shelly Paptan','Yashas A Ramanujapuram','Rik Sengupta','Hridbeg Choudhary','Divyam Saini','Asmi Amit Kumar','Sharayu Musale','Sejal Gupta','Prasham Agarwal' ]
+      list: ['Priyanshu Sinha', 'Mann Chhapolia', 'Krish Vora', 'Manan Gupta', 'Sai Vamsi Addepalli', 'Shelly Paptan', 'Yashas A Ramanujapuram', 'Rik Sengupta', 'Hridbeg Choudhary', 'Divyam Saini', 'Asmi Amit Kumar', 'Sharayu Musale', 'Sejal Gupta', 'Prasham Agarwal', 'Rishit Goel', 'Arnav Khandelwal']
   },
   1: {
       title: 'Painting',
       picture: ND,
       description: 'The Painting side at ADP still remains the best place to be for the top artists on campus (real ones know!). The current creations include 2 outside-Audi panels, 1 beautiful backdrop for the Main Audi, along with four Inside panels and several small panels, all made possible with tons of love, sweat and caffeine. They experiment with a range of mediums, including poster colors, charcoal, and soft pastels, often subject to the trends (and the whims) of the members!',
-      list: ['Navya Dhiman', 'Saanidhya Mittal', 'Rakshita Dave', 'Bhoomika Jain', 'Harsh Jha', 'Ria Munshi', 'Shruthi Adepu', 'Swarnima Bist', 'Devansh Agarwal', 'Kaustubh Tanwar', 'Lakshit Bansal', 'Urvi Singh', 'Rudr Bisht', 'Kamya Saraogi', 'Siya Bansal','Aditi Gupta','Navya Gupta','Tia Chibber','Aadaya Agarwal','Aditya Chordiya','Aniket Gupta ','Amey Sayankar','Janvi Pachisia','Jhanvi Matta']
+      list: ['Devansh Agarwal', 'Lakshit Bansal', 'Swarnima Bist', 'Kaustubh Tanwar', 'Urvi Singh', 'Rudr Bisht', 'Kamya Saraogi', 'Siya Bansal','Aditi Gupta','Navya Gupta','Tia Chibber','Aadaya Agarwal','Aditya Chordiya','Aniket Gupta','Amey Sayankar','Janvi Pachisia','Jhanvi Matta']
   },
   2: {
       title: 'Publicity',
       picture: REACT,
       description: 'The Publicity Side manages the social media for Oasis and APOGEE, and essentially takes care of all the marketing and branding needs for the fests, as the name suggests. It also creates both short-form (for Instagram) and long-form (for Inauguration) style videos, with the fast-expanding Video & 3D Team. Pub, as its better known, shares it\'s members with the finest technical clubs on campus, and their skills and expertise and put to good use with their newest pursuits and plans!',
-      list: ['Pranav Deshpande', 'Chavi Gupta', 'Aditya Raj Verma', 'Manan Jain', 'Nishkarsh Jain', 'Saaransh Jain', 'Arpit Maity', 'Sajal Batra', 'Krishnansh Sood',  'Aurvind Mohanty', 'Aditya Bhusari', 'Harsh Munje', 'Medhansh Adlakha', 'Nakshatara Garg','Aaryan Singh','Ram Avtar Dube','Palak Jha','Aarna Trivedi','Shreyansh Jain','Agastya Madaan','Umit Adalakha','Anshuman Pathak']
+      list: ['Aurvind Mohanty', 'Harsh Munje', 'Arpit Maity', 'Sajal Batra', 'Krishnansh Sood',  'Aditya Bhusari', 'Medhansh Adlakha', 'Nakshatara Garg','Aaryan Singh','Ram Avtar Dube','Palak Jha','Aarna Trivedi','Shreyansh Jain','Agastya Madaan','Umit Adalakha','Anshuman Pathak', 'Navam Harsha', 'Ishan Joshi', 'Atharva Tomar']
   },
   3: {
       title: 'Media',
       picture: REDUX,
       description: 'Media is the newest kid on the block for ADP, but it definitely doesn\'t act like one! With it being formally added only in 2022, it\'s taken huge strides by reaching out to and also forging key collaborations with prominent online and offline News Media houses, newspapers and influencers, contributing to the overall engagement and footfall of the fests.',
-      list: ['Sarthak Juneja', 'Archi Patel', 'Pranav Deshpande', 'Bhoomika Jain', 'Saanidhya Mittal', 'Ayush Jain', 'Rakshita Dave', 'Krish Vora', 'Siya Bansal', 'Swarnima Bist', 'Mann Chhapolia', 'Sai Vamsi Addepalli', 'Lakshit Bansal', 'Krishnansh Sood', 'Devansh Agarwal', 'Aditya Bhusari', 'Rudr Bisht', 'Medhansh Adlakha']
+      list: ['Krish Vora', 'Siya Bansal', 'Swarnima Bist', 'Mann Chhapolia', 'Sai Vamsi Addepalli', 'Lakshit Bansal', 'Krishnansh Sood', 'Devansh Agarwal', 'Aditya Bhusari', 'Rudr Bisht', 'Medhansh Adlakha', 'Aarna Trivedi', 'Arnav Khandelwal', 'Sharayu Musale', 'Amey Sayankar', 'Aniket Gupta', 'Prasham Agarwal', 'Aditi Gupta', 'Yashas A Ramanujapuram', 'Shreyansh Jain', 'Navam Harsha']
   }
 };
 
--- src/All_Lists/TechStackList.js
+++ src/All_Lists/TechStackList.js
@@ -1,15 +1,12 @@
-import HTML from '../assets/Cords/SV3.jpg';
-import BJ from '../assets/Cords/bj.jpeg';
-import ND from '../assets/Cords/ND.png';
-import REACT from '../assets/Cords/PD.png';
-import TAILWIND from '../assets/Cords/Aj.png';
-import REDUX from '../assets/Cords/Sj.jpeg';
-import GRADIOCJ from '../assets/Cords/CJ.png';
-import GRADIOAP from '../assets/Cords/sb3.jpeg';
-import GRADIOSH from '../assets/Cords/SH.png';
-import GRADIOSM from '../assets/Cords/SM.png';
+import AVD from '../assets/Cords/avd2.jpg';
+import TAILWIND from '../assets/Cords/ps2.jpg';
+import GRADIOHM from '../assets/Cords/hm2.png';
+import GRADIOSB from '../assets/Cords/sb3.jpeg';
+import GRADIOMC from '../assets/Cords/mann2.jpg';
+import GRADIOLB from '../assets/Cords/lb2.jpg';
 import pdd from '../assets/Cords/pdd2.png';
-import REDUX2 from '../assets/Cords/kv1.png';
+import REDUX2 from '../assets/Cords/kv2.jpg';
+import DA from '../assets/Cords/da2.jpg';
 
 const TechStackList = [
     {
@@ -20,27 +17,27 @@ const TechStackList = [
     },
     {
         id: 2,
-        image: BJ,
-        title: "Bhoomika Jain",
-        subtitle: "CoStAAn, APOGEE '25"
+        image: AVD,
+        title: "Aurvind Mohanty",
+        subtitle: "CoStAAn, APOGEE '26"
     },
     {
         id: 3,
         image: TAILWIND,
-        title: "Ayush Jain",
-        subtitle: "Structure Coord, Oasis '24"
+        title: "Priyanshu Sinha",
+        subtitle: "Structure Coord, Oasis '25       "
     },
     {
         id: 4,
-        image: ND,
-        title: "Navya Dhiman",
-        subtitle: "Painting Coord, Oasis '24"
+        image: DA,
+        title: "Devansh Agarwal",
+        subtitle: "Painting Coord, Oasis '25"
     },
     {
         id: 5,
-        image: pdd,
-        title: "Pranav Deshpande",
-        subtitle: "Publicity Coord, Oasis '24"
+        image: AVD,
+        title: "Aurvind Mohanty",
+        subtitle: "Publicity Coord, Oasis '25"
     },
     {
         id: 6,
@@ -50,26 +47,26 @@ const TechStackList = [
     },
     {
         id: 7,
-        image: GRADIOSH,
-        title: "Shlok Jaiswal",
-        subtitle: "Structure Coord, APOGEE '25"
+        image: GRADIOMC,
+        title: "Mann Chhapolia",
+        subtitle: "Structure Coord, APOGEE '26"
     },
     {
         id: 8,
-        image: GRADIOSM,
-        title: "Saanidhya Mittal",
-        subtitle: "Painting Coord, APOGEE '25"
+        image: GRADIOLB,
+        title: "Lakshit Bansal",
+        subtitle: "Painting Coord, APOGEE '26"
     },
     {
         id: 9,
-        image: GRADIOCJ,
-        title: "Chavi Gupta",
-        subtitle: "Publicity Coord, APOGEE '25"
+        image: GRADIOHM,
+        title: "Harsh Munje",
+        subtitle: "Publicity Coord, APOGEE '26"
     },
     {
         id: 10,
-        image: GRADIOAP,
-        title: "Archi Patel",
+        image: GRADIOSB,
+        title: "Siya Bansal",
         subtitle: "Media Coord, APOGEE '26"
     },
 
